b1526eecdabffdbfc47ed30efd3f5cfc2ec3cc0e
DavidJGGm <jonathangmz79@hotmail.com>
2026-02-21 11:46:32 -0600

    feat: Implement dynamic class loading demo with a refresh mechanism and a new Divisor class.

--- dynamic-class-loader/java-classgenerator-importer/demo/src/main/java/com/classgenerator/demo/DemoApplication.java
+++ dynamic-class-loader/java-classgenerator-importer/demo/src/main/java/com/classgenerator/demo/DemoApplication.java
@@ -1,15 +1,20 @@
 package com.classgenerator.demo;
 
-import java.io.File;
-import java.net.MalformedURLException;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.classgenerator.demo.interfaces.IOperable;
 import com.classgenerator.demo.models.WorkValue;
+
 import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
@@ -20,32 +25,82 @@ public class DemoApplication {
 	}
 
 	@PostConstruct
-	public void init() throws MalformedURLException, InterruptedException {
-		Thread.sleep(2000);
-		var file = new File(
-				"C:\\Users\\DGG\\Documents\\java-playground\\dynamic-class-loader\\java-classgenerator\\demo\\target\\classes");
+	public void init() throws InterruptedException, IOException {
+		/*
+		 * var file = new File(
+		 * "C:\Users\DGG\Documents\java-playground\dynamic-class-loader\java-classgenerator\demo\target\classes"
+		 * );
+		 */
 		var urls = new URL[] {
-				file.toURI().toURL()
+				// file.toURI().toURL()
+				URI.create(
+						"https://raw.githubusercontent.com/DavidJGG/java-playground/main/dynamic-class-loader/java-classgenerator/demo/target/classes/")
+						.toURL()
 		};
 
-		ejecutar(urls, "com.classgenerator.demo.generados.Sumador");
-		ejecutar(urls, "com.classgenerator.demo.generados.Restador");
-		ejecutar(urls, "com.classgenerator.demo.generados2.Multiplicador");
-		ejecutar(urls, "com.classgenerator.demo.generados2.Divisor");
+		try {
+			while (true) {
+				ejecutar(urls, "com.classgenerator.demo.generados.Sumador");
+				ejecutar(urls, "com.classgenerator.demo.generados.Restador");
+				ejecutar(urls, "com.classgenerator.demo.generados2.Multiplicador");
+				ejecutar(urls, "com.classgenerator.demo.generados2.Divisor");
+				System.out.println();
+				System.out.println();
+				System.out.println();
+				Thread.sleep(2000);
+			}
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException | NoSuchMethodException | SecurityException e) {
+			e.printStackTrace();
+		} finally {
+			classLoader.close();
+		}
 
 	}
 
-	private void ejecutar(URL[] urls, String className) {
-		try (URLClassLoader classLoader = new URLClassLoader(urls, IOperable.class.getClassLoader())) {
-			var sumadorClass = classLoader.loadClass(className);
-			var sumador = (IOperable<? extends Number, WorkValue>) sumadorClass.getDeclaredConstructor()
-					.newInstance();
-			System.out.println("-----------------------------------------");
-			System.out.println(sumador.whoami());
-			System.out.println(sumador.ejecutar(new WorkValue(3, 5)));
-			System.out.println("-----------------------------------------");
-		} catch (Exception e) {
-			e.printStackTrace();
+	private void ejecutar(URL[] urls, String className)
+			throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException, NoSuchMethodException, SecurityException {
+		URLClassLoader classLoader = getClassLoader(urls);
+		var sumadorClass = classLoader.loadClass(className);
+		var sumador = (IOperable<? extends Number, WorkValue>) sumadorClass.getDeclaredConstructor()
+				.newInstance();
+		System.out.println("-----------------------------------------");
+		System.out.println(sumador.whoami());
+		System.out.println(sumador.ejecutar(new WorkValue(3, 5)));
+		System.out.println("-----------------------------------------");
+	}
+
+	private URLClassLoader classLoader = null;
+
+	private URLClassLoader getClassLoader(URL[] urls) {
+		try {
+			if (classLoader == null) {
+				this.classLoader = new URLClassLoader(urls, IOperable.class.getClassLoader());
+				return this.classLoader;
+			}
+
+			var refreshFilePath = "C:\\Users\\" + System.getProperty("user.name")
+					+ "\\Documents\\java-playground\\dynamic-class-loader\\java-classgenerator-importer\\refrescar";
+			var lines = Files.readAllLines(Path.of(refreshFilePath), StandardCharsets.UTF_8);
+			return lines.stream().filter(line -> !line.isBlank() && line.startsWith("refrescar=")).map(line -> {
+				if (line.split("=")[1].equals("true")) {
+					try {
+						// Eliminando refrescar=true del archivo
+						Files.delete(Path.of(refreshFilePath));
+						// Agregando refrescar=false al archivo
+						Files.write(Path.of(refreshFilePath), "refrescar=false".getBytes());
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					System.out.println("*** Refrescando ClassLoader ***");
+					this.classLoader = new URLClassLoader(urls, IOperable.class.getClassLoader());
+				}
+				return this.classLoader;
+			}).findFirst().orElse(this.classLoader);
+
+		} catch (IOException e) {
+			throw new IllegalStateException(e);
 		}
 	}
 
